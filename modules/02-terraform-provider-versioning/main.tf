d298fe07baa3532705a7c6572dfa4b28e6b1346e
akabanov <akabanov@bravurasolutions.com>
2022-09-20 17:23:45 +1200

    02 terraform-provider-versioning #1

---
+++ modules/02-terraform-provider-versioning/main.tf
@@ -0,0 +1,19 @@
+resource "random_pet" "pet-name" {
+  length = 5
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = random_pet.pet-name.id
+  tags = {
+    public-bucket = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket-acl" {
+  bucket = aws_s3_bucket.my-bucket.id
+  acl = "private"
+}
---
+++ modules/02-terraform-provider-versioning/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+
+  required_version = ">= 1.1"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
